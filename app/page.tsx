70e9becffe82853fa41b715ab9d8246b0b08d629
CenCiviC <rudqls513@gmail.com>
2025-04-13 18:14:12 +0900

    feat : home 화면 문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <TopBar hasMenuBtn={false} />
       <div className="flex flex-1 w-full">
         <div className="flex bg-primary-50 w-full h-full">
-          description of the imkyungbin
+          블로그를 소개하는 페이지입니다.
         </div>
       </div>
     </>
